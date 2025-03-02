78c2dfc5ff38cc31dbf536ebadce0a5b40cc3428
mohdilzam <mohd.ilzam60@gmail.com>
2025-03-02 11:28:22 +0700

    interface & database JSON

--- doctorDb.js
+++ doctorDb.js
@@ -1,2 +1,11 @@
 const fs = require('fs');
 const readline = require('readline');
+
+// Membuat interface untuk interaksi dengan terminal
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Path ke file database JSON
+const dbPath = './doctors.json';
